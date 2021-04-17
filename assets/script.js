7565284f69919cba9c7222e6ec3407717b451059
Tom Erickson <ericksont91@gmail.com>
2021-04-17 12:09:38 -0400

    testing

---
+++ assets/script.js
@@ -0,0 +1,52 @@
+var imageContainer = document.querySelector(".img-container");
+var dco = document.querySelector(".One")
+var dct = document.querySelector(".Two")
+var dcx = document.querySelector(".Three")
+var dcf = document.querySelector(".Four")
+var BODZ = document.querySelector(".Bode")
+var colorsArray = ['rgb(12, 172, 212)', 'white', 'red', 'blue', 'url(https://images.unsplash.com/photo-1583511655857-d19b40a7a54e)']
+var num = 0
+
+imageContainer.addEventListener("click", function(event) {
+  event.stopPropagation();
+  var element = event.target;
+
+  if (element.matches("img")) {
+    var state = element.getAttribute("data-state");
+
+    if (state === "still") {
+      element.dataset.state = "animate";
+      element.setAttribute("data-state", "animate");
+      element.setAttribute("src", element.dataset.animate);
+    } else {
+      element.dataset.state = "still";
+      element.setAttribute("src", element.dataset.still);
+    }
+  }
+});
+
+
+  function changeColor(event) {
+    event.currentTarget.setAttribute(
+        "style",
+        "background:"+colorsArray[num]+"; background-size: 100vw 100vh;",
+    );
+    if(num >= 4){
+      imageContainer.style.display = 'none'
+    }
+    num++
+  }
+
+
+  BODZ.addEventListener("click", changeColor)
+
+  
+
+//  Listen for any clicks within the img-container div
+// Get the current value of the image's data-state attribute
+      // Change the data-state attribute's value
+      // There are two different ways this attribute can be set
+      // Update the image's source to the string being stored in the data-animate attribute
+      // Change the attributes back to their non-animated values
+// I want the background to change with each click and the pictures to enlage to the .gifs when clicked.  
+// When all four are clicked, Toto's Hold the Line Plays
